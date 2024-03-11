5251b36f15a0889406345f26eefe6967ab5e8791
yyyy-01 <100837114+yyyy-01@users.noreply.github.com>
2024-03-11 17:34:37 +0800

    changed text in email button

--- app/src/main/java/com/thengyuanyuan/kotlin_demo_2024/MainActivity.kt
+++ app/src/main/java/com/thengyuanyuan/kotlin_demo_2024/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "I changed some text here", Snackbar.LENGTH_LONG)
                     .setAction("Action", null)
                     .setAnchorView(R.id.fab).show()
         }
